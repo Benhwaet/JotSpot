3f16b2fea61b9cf57817f7dfc1148ce101a8e5fa
BenHwaet <benmdesroches@gmail.com>
2023-10-09 15:07:05 -0300

    added install.js to html as script, implemented asset caching in src service worker file.

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,6 +1,6 @@
-const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
-const { registerRoute } = require('workbox-routing');
+const { warmStrategyCache } = require('workbox-recipes');
+const { CacheFirst, StaleWhileRevalidate } = require('workbox-strategies');
+const { registerRoute, setDefaultHandler, setCatchHandler } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
 const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
@@ -19,12 +19,29 @@ const pageCache = new CacheFirst({
   ],
 });
 
-warmStrategyCache({
-  urls: ['/index.html', '/'],
-  strategy: pageCache,
-});
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
+const fallbackHtml = '/index.html';
+const fallbackImg = '/assets/images/logo.png';
+
+warmStrategyCache({
+  urls: [fallbackHtml, fallbackImg],
+  strategy: pageCache,
+});
+
+setDefaultHandler(new StaleWhileRevalidate());
+
+setCatchHandler(async ({ request }) => {
+switch (request.destination) {
+  case 'document':
+    return caches.match(fallbackHtml);
+  case 'image':
+    return caches.match(fallbackImg);
+  default:
+    return Response.error();
+  }
+});
+
 registerRoute();
